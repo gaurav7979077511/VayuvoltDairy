36f1ce0a5727f0a04a565dc45f89c99e4739e6e3
Kumar Gaurav <129366506+gaurav7979077511@users.noreply.github.com>
2026-04-02 00:56:30 +0530

    Update VayuvoltDairy.py

--- VayuvoltDairy.py
+++ VayuvoltDairy.py
@@ -3154,15 +3154,21 @@ else:
                 """
             #----
             DailyMilkPattern_html = ""
-
-            if "DailyMilkPattern" in r and pd.notna(r["DailyMilkPattern"]) and str(r["DailyMilkPattern"]).strip():
-                DailyMilkPattern_html = f"""
-                <div style="font-size:12px;opacity:0.9;margin-top:4px;">
-                    ❌ No milk: {r["DailyMilkPattern"]}
-                </div>
-                """
-            else:
-                DailyMilkPattern_html = "<span style='font-size:11px;opacity:.9;'>No gaps</span>"
+                if "DailyMilkPattern" in r and pd.notna(r["DailyMilkPattern"]) and r["DailyMilkPattern"]:
+                    for d in str(r["DailyMilkPattern"]).split(","):
+                        DailyMilkPattern_html += f"""
+                        <span style="
+                            padding:2px 6px;
+                            background:#ffffff33;
+                            border-radius:6px;
+                            font-size:11px;
+                            margin-right:4px;
+                            margin-top:4px;
+                            display:inline-block;
+                        ">{d.strip()}</span>
+                        """
+                else:
+                    DailyMilkPattern_html = "<span style='font-size:11px;opacity:.9;'>No daily_pattern</span>"
 
             card_html = f"""
             <div style="
